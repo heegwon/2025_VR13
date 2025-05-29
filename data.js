aea9408923687343c7a2c61f3da29e40faabd772
heegwon <heegwon0301@gmail.com>
2025-05-29 15:24:38 +0900

    A
    TK

---
+++ data.js
@@ -0,0 +1,347 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-",
+      "name": "옥외",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1536,
+      "initialViewParameters": {
+        "yaw": -1.5318078372066601,
+        "pitch": -0.06818127917807537,
+        "fov": 1.5396019870895838
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.1154919658235869,
+          "pitch": 0.1949748451665947,
+          "rotation": 0,
+          "target": "1--"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.537967976907103,
+          "pitch": -0.36378121299964405,
+          "title": "건축물 투어 소개",
+          "text": "신비로운 섬에 위치한 휴양을 위해 만들어진 주거입니다.<div><br></div><div>이섬에는 신비의 동물이 살고 있습니다. 이 동물은 착한 사람 눈에만 보이며, 이 동물과 교감을 나누게 되면 병들지 않고, 늙지 않고 살수 있다는 전설이 있습니다.</div><div><br></div><div>건축물을 둘러보면서 이 신비의 동물을 찾아 보세요.</div>"
+        },
+        {
+          "yaw": -0.2722616421812383,
+          "pitch": -0.3961451404642702,
+          "title": "Title",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "1--",
+      "name": "건축물 정면",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1536,
+      "initialViewParameters": {
+        "yaw": -0.11067349831323092,
+        "pitch": 0.011586064315363132,
+        "fov": 1.5396019870895838
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.6384519330589189,
+          "pitch": 0.10218870318777284,
+          "rotation": 0,
+          "target": "2-"
+        },
+        {
+          "yaw": 0.06141563874165534,
+          "pitch": 0.11724301701132234,
+          "rotation": 0,
+          "target": "3-"
+        },
+        {
+          "yaw": 2.8123925933936498,
+          "pitch": 0.17714004617840295,
+          "rotation": 0,
+          "target": "0-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.0856875572912283,
+          "pitch": -0.40221967290848326,
+          "title": "Title",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "2-",
+      "name": "주방",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1536,
+      "initialViewParameters": {
+        "yaw": -0.007688597580141732,
+        "pitch": 0,
+        "fov": 1.5396019870895838
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.588961222697627,
+          "pitch": 0.2778594560977439,
+          "rotation": 0,
+          "target": "3-"
+        },
+        {
+          "yaw": -2.4364075023912157,
+          "pitch": 0.32863387039216363,
+          "rotation": 0,
+          "target": "1--"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.4779305179888116,
+          "pitch": -0.2966740197532687,
+          "title": "Title",
+          "text": "Text"
+        },
+        {
+          "yaw": 1.1678347447652992,
+          "pitch": -0.36025752728054883,
+          "title": "Title",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "3-",
+      "name": "거실",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1536,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.4878190218951168,
+          "pitch": 0.015590132235049126,
+          "rotation": 0,
+          "target": "2-"
+        },
+        {
+          "yaw": 1.5606735550984983,
+          "pitch": 0.1389812575296805,
+          "rotation": 0,
+          "target": "4-"
+        },
+        {
+          "yaw": 0.8668839345638055,
+          "pitch": 0.1230180820447444,
+          "rotation": 0,
+          "target": "4-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.950746923010847,
+          "pitch": -0.4003099962433083,
+          "title": "Title",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "4-",
+      "name": "침실",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1536,
+      "initialViewParameters": {
+        "yaw": 0.06860077668193654,
+        "pitch": -0.003527152318650195,
+        "fov": 1.5396019870895838
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.837404546511812,
+          "pitch": 0.04403474410039898,
+          "rotation": 0,
+          "target": "5-"
+        },
+        {
+          "yaw": -1.1417603380806476,
+          "pitch": 0.22917202723147767,
+          "rotation": 0,
+          "target": "3-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.8804712462819708,
+          "pitch": -0.3783271978338476,
+          "title": "Title",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "5-",
+      "name": "욕실",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1536,
+      "initialViewParameters": {
+        "yaw": -0.05463290256769149,
+        "pitch": 0.027365132867970487,
+        "fov": 1.5396019870895838
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.6717208756869297,
+          "pitch": 0.42956797565132554,
+          "rotation": 0,
+          "target": "4-"
+        },
+        {
+          "yaw": -1.6351081365368696,
+          "pitch": 0.024347842727518554,
+          "rotation": 0,
+          "target": "3-"
+        },
+        {
+          "yaw": -2.8399593651964565,
+          "pitch": 0.10269840579812595,
+          "rotation": 0,
+          "target": "0-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.8640244193536297,
+          "pitch": -0.412223379993506,
+          "title": "Title",
+          "text": "Text"
+        },
+        {
+          "yaw": 2.4177838332098904,
+          "pitch": -0.2127436046477058,
+          "title": "Title",
+          "text": "Text"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
